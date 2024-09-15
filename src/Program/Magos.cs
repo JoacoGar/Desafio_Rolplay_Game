819bcce36072611f1de541bd2f4ded48c799e07a
JoacoGar <joaquins.garcia@correo.ucu.edu.uy>
2024-09-15 13:39:11 -0300

    Se crea Magos, SIN METODOS

---
+++ src/Program/Magos.cs
@@ -0,0 +1,22 @@
+namespace Program;
+
+public class Mago
+{
+    public string Nombre { get; set; }
+    public int Puntos_de_salud { get; set; }
+    public int Puntos_de_ataque { get; set; }   
+    public int Puntos_de_defensa { get; set; }
+    public List<Armas> Arma { get; set; }
+    public List <Armaduras> Armadura { get; set; }
+    
+    public Mago(string nombre, int salud)
+    {
+        Nombre = nombre;
+        Puntos_de_salud = salud;
+        Puntos_de_ataque = 5;  // Declaro unos puntos de ataque y de defensa iniciales, dado que los mismos seran otorgados por los distintos elementos.
+        Puntos_de_defensa = 0;
+        Arma = new List<Armas>();
+        Armadura = new List<Armaduras>();
+    } 
+}   
+
