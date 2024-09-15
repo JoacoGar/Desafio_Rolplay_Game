319b962cb4aae79cbce002f1a071c845c011582d
JoacoGar <joaquins.garcia@correo.ucu.edu.uy>
2024-09-15 12:47:46 -0300

    Se agregan los tipos de los elementos

---
+++ src/Program/Tipos.cs
@@ -0,0 +1,16 @@
+﻿public interface IPersonaje {
+    string nombre_personaje {get;set;}
+    int vida_personaje {get;set;}
+    int poder_de_ataque {get;set;}
+    int puntos_de_defensa {get;set;}
+    void Atacar();
+    void Curar();
+}
+
+public interface IElementos {
+    string nombre_elemento {get;set;}
+    int poder_de_ataque {get;set;}
+    int poder_de_defensa {get;set;}
+}
+    
+    
