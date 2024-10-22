a2a729349fbea133fa4209e0d6ba214947fde76b
Joaco Balian <joaquin.balian@correo.ucu.edu.uy>
2024-10-21 23:07:37 -0300

    Test

--- src/Library/Encuentro.cs
+++ src/Library/Encuentro.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 
 namespace Library;
 
@@ -15,104 +13,104 @@ public class Encuentro
         Malos = malos;
     }
 
-            public void DoEncounter()
+    public void DoEncounter()
+    {
+        if (!Malos.Any() || !Heroes.Any())
         {
-            if (!Malos.Any() || !Heroes.Any())
-            {
-                Console.WriteLine("No hay encuentro");
-                return;
-            }
-
-            bool end = false;
-
-            while (!end)
-            {
-                
-                Console.WriteLine("Atacan los malos");
-                TurnoEnemigos();
-
-                if (Heroes.Count == 0)
-                {
-                    Console.WriteLine("Ganaron los malos");
-                    end = true;
-                    break;
-                }
-
-                Console.WriteLine("Atacan los Heroes");
-                TurnoHeroes();
-
-                if (Malos.Count == 0)
-                {
-                    Console.WriteLine("Ganaron los heroes");
-                    end = true;
-                    break;
-                }
-
-            }
+            Console.WriteLine("No hay encuentro");
+            return;
         }
 
-        public void TurnoEnemigos()
+        bool end = false;
+
+        while (!end)
         {
-            int contador = 0;
 
-            foreach (var enemigos in Malos.ToList()) 
+            Console.WriteLine("Atacan los malos");
+            TurnoEnemigos();
+
+            if (Heroes.Count == 0)
             {
-                Heroes hero = Heroes[contador];
-                int restarVida = enemigos.Puntos_de_ataque;
-                hero.Puntos_de_salud -= restarVida;
-
-                Console.WriteLine($"{enemigos.Nombre} atacó a {hero.Nombre}, restandole {restarVida} puntos de vida.");
-
-                if (hero.Puntos_de_salud <= 0)
-                {
-                    Console.WriteLine($"{hero.Nombre} fue abatido.");
-                    Heroes.Remove(hero);
-                }
-
-                contador = (contador + 1) % Heroes.Count; 
-        }
-
-        public void TurnoHeroes()
-        {
-            foreach (var hero in Heroes.ToList())
-            {
-                foreach (var enemigo in Malos.ToList()) 
-                {
-                    int restarVida = hero.Puntos_de_ataque;
-                    enemigo.Puntos_de_salud -= restarVida;
-
-                    Console.WriteLine($"{hero.Nombre} atacó a {enemigo.Nombre}, restandole {restarVida} puntos de vida.");
-
-                    if (enemigo.Puntos_de_salud <= 0)
-                    {
-                        Console.WriteLine($"{enemigo.Nombre} ha sido eliminado.");
-                        hero.VP += enemigo.VP;
-                        Malos.Remove(enemigo);
-                    }
-
-                    if (hero.VP >= 5)
-                    {
-                        CurarHeroe(hero);
-                    }
-
-                    if (Malos.Count == 0)
-                    {
-                        break; // Termina el turno si no quedan enemigos
-                    }
-                }
-
-                if (Malos.Count == 0)
-                {
-                    break; // Termina el turno si no quedan enemigos
-                }
+                Console.WriteLine("Ganaron los malos");
+                end = true;
+                break;
             }
-        }
 
-        public void CurarHeroe(Heroes hero)
-        {
-            hero.Puntos_de_salud = 100;
-            hero.VP = 0; 
-            Console.WriteLine($"El héroe {hero.Nombre} ha recargado su vida al 100 y sus puntos de victoria han sido reseteados.");
+            Console.WriteLine("Atacan los Heroes");
+            TurnoHeroes();
+
+            if (Malos.Count == 0)
+            {
+                Console.WriteLine("Ganaron los heroes");
+                end = true;
+                break;
+            }
+
         }
     }
+
+    public void TurnoEnemigos()
+    {
+        int contador = 0;
+
+        foreach (var enemigos in Malos.ToList())
+        {
+            Heroes hero = Heroes[contador];
+            int restarVida = enemigos.Puntos_de_ataque;
+            hero.Puntos_de_salud -= restarVida;
+
+            Console.WriteLine($"{enemigos.Nombre} atacó a {hero.Nombre}, restandole {restarVida} puntos de vida.");
+
+            if (hero.Puntos_de_salud <= 0)
+            {
+                Console.WriteLine($"{hero.Nombre} fue abatido.");
+                Heroes.Remove(hero);
+            }
+
+            contador = (contador + 1) % Heroes.Count;
+        }
+    }
+
+    public void TurnoHeroes()
+    {
+        foreach (var hero in Heroes.ToList())
+        {
+            foreach (var enemigo in Malos.ToList())
+            {
+                int restarVida = hero.Puntos_de_ataque;
+                enemigo.Puntos_de_salud -= restarVida;
+
+                Console.WriteLine($"{hero.Nombre} atacó a {enemigo.Nombre}, restandole {restarVida} puntos de vida.");
+
+                if (enemigo.Puntos_de_salud <= 0)
+                {
+                    Console.WriteLine($"{enemigo.Nombre} fue abatido.");
+                    hero.VP += enemigo.VP;
+                    Malos.Remove(enemigo);
+                }
+
+                if (hero.VP >= 5)
+                {
+                    CurarHeroe(hero);
+                }
+
+                if (Malos.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            
+        }
+    }
+
+
+    public void CurarHeroe(Heroes hero)
+        {
+            hero.Puntos_de_salud = 100;
+            hero.VP = 0;
+            Console.WriteLine($"El héroe {hero.Nombre} se ha curado.");
+        }
+    
 }
+
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -13,7 +13,8 @@ public class Program
 
 
             Encuentro encuentro = new Encuentro(new List<Heroes> { hero }, new List<Malos> { enemy, enem });
-            hero.Puntos_de_ataque = 150; 
+            hero.Puntos_de_ataque = 150;
+            enem.Puntos_de_ataque = 200;
 
             encuentro.DoEncounter();
         }
