3b83264638ed4d1b0edeeec90f06d1a469770314
Nicolas Perez <nicolas.pgarcia@correo.ucu.edu.uy>
2024-10-21 23:43:45 -0300

    tests y ejemplo
    7 test
    ejemplo en program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,21 +1,39 @@
 using Library;
 
-namespace Program;
+namespace Programa;
 
-public class Program
+public class Programa
 {
-    static void Main(string[] args) // Elimina el segundo Main y ajusta el formato.
+    static void Main() 
     {
         {
-            Heroes hero = new Heroes("Aragorn");
-            Malos enemy = new Malos("Orc", 3);
-            Malos enem = new Malos("Orce", 4);
+            Heroes heroe1 = new Heroes("Gabriel");
+            Heroes heroe2 = new Heroes("Jose");
+            
+            Malos enemigo1 = new Malos("ElPata", 2);
+            Malos enemigo2 = new Malos("Valentina", 5);
+            Malos enemigo3 = new Malos("ElJota", 8);
+            
+            heroe1.Puntos_de_ataque = 120;
+            heroe1.Puntos_de_defensa = 20;
 
+            heroe2.Puntos_de_ataque = 80;
+            heroe2.Puntos_de_defensa = 40;
+            
+            enemigo1.Puntos_de_ataque = 30;
+            enemigo1.Puntos_de_defensa = 10;
 
-            Encuentro encuentro = new Encuentro(new List<Heroes> { hero }, new List<Malos> { enemy, enem });
-            hero.Puntos_de_ataque = 150;
-            enem.Puntos_de_ataque = 20;
+            enemigo2.Puntos_de_ataque = 40;
+            enemigo2.Puntos_de_defensa = 15;
 
+            enemigo3.Puntos_de_ataque = 60;
+            enemigo3.Puntos_de_defensa = 25;
+            
+            Encuentro encuentro = new Encuentro(
+                new List<Heroes> { heroe1, heroe2 }, 
+                new List<Malos> { enemigo1, enemigo2, enemigo3 }
+            );
+            
             encuentro.DoEncounter();
         }
     }
--- test/Test/UnitTest1.cs
+++ test/Test/UnitTest1.cs
@@ -1,15 +1,96 @@
-namespace Test;
+using Library; 
 
-public class Tests
+namespace Test
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [Test]
+        public void TestArma()
+        {
+            
+            var nombre = "Espada";
+            var ataque = 50;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            
+            var arma = new Arma(nombre, ataque);
+
+            
+            Assert.AreEqual(nombre, arma.Nombre);
+            Assert.AreEqual(ataque, arma.Ataque);
+        }
+
+        [Test]
+        public void TestAtaqueyDefensa()
+        {
+            var nombre = "Espada con escudo";
+            var ataque = 40;
+            var defensa = 20;
+            
+            var item = new AtaqueyDefensa(nombre, ataque, defensa);
+            
+            Assert.AreEqual(nombre, item.Nombre);
+            Assert.AreEqual(ataque, item.Ataque);
+            Assert.AreEqual(defensa, item.Defensa);
+        }
+
+        [Test]
+        public void TestDefensa()
+        {
+            
+            var nombre = "Escudo";
+            var escudo = 30;
+
+            
+            var defensa = new Defensa(nombre, escudo);
+
+            
+            Assert.AreEqual(nombre, defensa.Nombre);
+            Assert.AreEqual(escudo, defensa.Escudo);
+        }
+
+        [Test]
+        public void TestHeroes()
+        {
+            
+            var nombre = "Héroe";
+            
+            var heroe = new Heroes(nombre);
+            
+            Assert.AreEqual(nombre, heroe.Nombre);
+            Assert.AreEqual(0, heroe.VP);
+            Assert.AreEqual(100, heroe.Puntos_de_salud);
+        }
+
+        [Test]
+        public void TestHeroesPV()
+        {
+            var heroe = new Heroes("Héroe");
+            
+            heroe.GanarVP(3);
+            
+            Assert.AreEqual(3, heroe.VP);
+        }
+
+        [Test]
+        public void TestMalos()
+        {
+            var nombre = "Villano";
+            var vp = 5;
+            
+            var malo = new Malos(nombre, vp);
+            
+            Assert.AreEqual(nombre, malo.Nombre);
+            Assert.AreEqual(vp, malo.VP);
+            Assert.AreEqual(100, malo.Puntos_de_salud);
+        }
+
+        [Test]
+        public void TestEncuentro()
+        {
+            var encuentro = new Encuentro(new List<Heroes>(), new List<Malos>());
+            
+            encuentro.DoEncounter();
+
+        }
     }
 }
